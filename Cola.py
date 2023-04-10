8bc95a4d10c0574b9fc04474f91de534162854d9
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-10 17:13:13 +0200

    cambiios necesarios en el imprimir

--- Cola.py
+++ Cola.py
@@ -40,18 +40,29 @@ class Cola:
             self.items[0]
             self.items[1]
             self.items[2]
-            print(f"[ Barbero , {self.items[0].estado} - Cliente {self.items[0].numCliente} , {self.items[1].estado} - Cliente {self.items[1].numCliente} , {self.items[2].estado} - Cliente {self.items[2].numCliente} ]")
+            self.items[3]
+
+            print(f"[ Cliente {self.items[0].getNumCliente()} - {self.items[0].getEstado()} , Cliente {self.items[1].getNumCliente()} - {self.items[1].getEstado()} , Cliente {self.items[2].getNumCliente()} - {self.items[2].getEstado()} - Cliente {self.items[3].getNumCliente()} - {self.items[3].getEstado()} ]")
         except IndexError:
             try:
                 self.items[0]
                 self.items[1]
-                print(f"[ Barbero , {self.items[0].estado} - Cliente {self.items[0].numCliente} , {self.items[1].estado} - Cliente {self.items[1].numCliente} , Silla 3 - Vacio ]")
+                self.items[2]
+
+                print(f"[ Cliente {self.items[0].getNumCliente()} - {self.items[0].getEstado()} , Cliente {self.items[1].getNumCliente()} - {self.items[1].getEstado()} , Cliente {self.items[2].getNumCliente()} - {self.items[2].getEstado()} , Silla 3 - Vacio ]")
             except IndexError:
                 try:
                     self.items[0]
-                    print(f"[ Barbero , {self.items[0].estado} - Cliente {self.items[0].numCliente} , Silla 2 - Vacio , Silla 3 - Vacio ]")
+                    self.items[1]
+
+                    print(f"[ Cliente {self.items[0].getNumCliente()} - {self.item[0].getEstado()}, Cliente {self.items[1].getNumCliente()} - {self.item[1].getEstado()} , Silla 2 - Vacio , Silla 3 - Vacio ]")
                 except IndexError:
-                    print("[ Barbero , Silla 1 - Vacio , Silla 2 - Vacio , Silla 3 - Vacio ]")
+                    try:
+                        self.items[0]
+
+                        print(f"[ Cliente {self.items[0].getNumCliente()} - {self.item[0].getEstado()} , Silla 2 - Vacio , Silla 3 - Vacio ]")
+                    except IndexError:
+                        print("[ Nadie con Barbero , Silla 1 - Vacio , Silla 2 - Vacio , Silla 3 - Vacio ]")
 
     def count(self):
         """ Metodo que devuelve el numero de clientes en la cola """
