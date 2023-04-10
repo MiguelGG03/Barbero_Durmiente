ad0dd843512dddbadef525eba1267a13f412190b
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-10 10:52:20 +0200

    ns seguir

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,24 @@
+import random
+
 class Cliente:
 
-    def __init__(self):
-        pass
+    # Constructor de la clase
+    def __init__(self,numCliente,estado):
+        self.numCliente=numCliente
+        self.estado=estado
+        self.tiempoEsperado=0
+        self.tiempoDeEspera=random.randrange(5,25)
+
+    # Getters and Setters
+    def getNumCliente(self):
+        return self.numCliente
+    
+    def setNumCliente(self,x:int):
+        self.numCliente=x
+
+    def getEstado(self):
+        return self.estado
+    
+    def setEstado(self,x:str):
+        self.estado=x
+        
