2a459c3aef2f7c71d40aa080adf28095d5ac5ee4
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-10 16:52:39 +0200

    Rehaciendo la cola

--- Lanzador.py
+++ Lanzador.py
@@ -22,3 +22,17 @@ def main():
                     cola.encolar(Cliente(j,"Con Barbero"))
                 elif cola.count() == 1:
                     cola.encolar(Cliente(j,"Esperando en Silla 1"))
+                elif cola.count() == 2:
+                    cola.encolar(Cliente(j,"Esperando en Silla 2"))
+                elif cola.count() == 3:
+                    cola.encolar(Cliente(j,"Esperando en Silla 3"))
+                else:
+                    print("No hay sillas disponibles, el cliente se va\n")
+                    j -= 1
+
+            print("| Posiciones posibles : [ Barbero , Silla 1 , Silla 2 , Silla 3 ] |\n")
+            print("\n---------------------------------------------------------------\n")
+            print(f"| Posiciones ocupadas : {cola.imprimir()} |")
+
+        if not cola.estaVacia():
+            cola.first().getTiempoDeEspera() == cola.first.getTiempoEsperado()
